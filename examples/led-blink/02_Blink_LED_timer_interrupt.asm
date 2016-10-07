4885071d1b1d0f70662931630989acb47a719f04
Sebastian Krüger <krueger.oh@googlemail.com>
2016-10-07 23:53:09 +0200

    Blink LED in Timer 2

--- examples/led-blink/02_Blink_LED_timer_interrupt.asm
+++ examples/led-blink/02_Blink_LED_timer_interrupt.asm
@@ -1,5 +1,8 @@
 ;*******************************************************************************
-;*                           Blink LED Example                                 *
+;*                        02 Blink LED Example                                 *
+;* This example work with timer interrupt to create blink delay                *
+;* LED Status switch. PIC will drive at 2 Mhz = 2 uS per Cycle                 *
+;*                                                                             *
 ;*                                 v1.00                                       *
 ;*******************************************************************************
 ;*           Hardware: PIC12F1840                                              *
@@ -7,16 +10,25 @@
 ;*******************************************************************************
 
 ;===============================================================================
-;       Hardware Check
+;		Hardware Check
 ;===============================================================================
 
-IFNDEF __12LF1840
+IFNDEF __12F1840
    MESSG "Processor-header file mismatch.  Verify selected processor."
 ENDIF
 
+; CONFIG1
+; __CONFIG _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF 
+;      & _CP_OFF & _CPD_OFF & _BOREN_ON & _CLKOUTEN_OFF & _IESO_ON & _FCMEN_ON
+__CONFIG 0x8007, B'1111111110100100'
+
+; CONFIG2
+; __CONFIG _CONFIG2, _WRT_OFF & _PLLEN_ON & _STVREN_ON & _BORV_LO & _LVP_ON
+__CONFIG 0x8008, B'1111111111111111'
+
 ;-------------------------------------------------------------------------------
-;Pin connection
-;----------------------------------------
+;		Pin connection
+;-------------------------------------------------------------------------------
 ;RA0 --> N/C
 ;RA1 --> N/C
 ;RA2 --> LED
@@ -27,97 +39,105 @@ ENDIF
 
 
 ;===============================================================================
-;       Register Definitions
+;		Register Definitions
 ;===============================================================================
 
-W                EQU  H'0000'
-F                EQU  H'0001'
 
-;-----Bank0------------------
-INDF0            EQU  H'0000'
-INDF1            EQU  H'0001'
-PCL              EQU  H'0002'
-STATUS           EQU  H'0003'
-FSR0             EQU  H'0004'
-FSR0L            EQU  H'0004'
-FSR0H            EQU  H'0005'
-FSR1             EQU  H'0006'
-FSR1L            EQU  H'0006'
-FSR1H            EQU  H'0007'
-BSR              EQU  H'0008'
-WREG             EQU  H'0009'
-PCLATH           EQU  H'000A'
-INTCON           EQU  H'000B'
-PORTA            EQU  H'000C'
-PIR1             EQU  H'0011'
-PIR2             EQU  H'0012'
-TMR0             EQU  H'0015'
-TMR1             EQU  H'0016'
-TMR1L            EQU  H'0016'
-TMR1H            EQU  H'0017'
-T1CON            EQU  H'0018'
-T1GCON           EQU  H'0019'
-TMR2             EQU  H'001A'
-PR2              EQU  H'001B'
-T2CON            EQU  H'001C'
-CPSCON0          EQU  H'001E'
-CPSCON1          EQU  H'001F'
+INTCON			EQU H'00b'			; Bank 0
+PORTA			EQU H'00C'			; Bank 0
+PIR1			EQU H'011'			; Bank 0
+TMR2			EQU H'01a'			; Bank 0
+PR2				EQU H'01b'			; Bank 0
+T2CON			EQU H'01c'			; Bank 0
+TRISA			EQU H'08c'			; Bank 1
+PIE1			EQU H'091'			; Bank 1
+PIE2			EQU H'092'			; Bank 1
+OSCCON			EQU H'099'			; Bank 1
+LATA			EQU H'10C'			; Bank 2
+ANSELA			EQU H'18c'			; Bank 3
 
-;----- PORTA Bits -----------------------------------------------------
-RA2              EQU  H'0002'
+;----- Register Function Bits --------------------------------------------------
+TMR2IE			EQU D'1'			; Timer2 Interrupt enable
+TMR2IF			EQU D'1'			; Timer2 Interrupt flag
+TMR2ON			EQU D'2'			; Timer2 on/off
 
-;----- PIE1 Bits -----------------------------------------------------
-TMR2IE           EQU  H'0001'  
+;----- PORTA Bits --------------------------------------------------------------
+RA2				EQU D'2'
+
 
 ;===============================================================================
-;       Variable Definitions
+;		Configuration
 ;===============================================================================
 
-;===============================================================================
-;       Configuration
-;===============================================================================
-
-      	     __CONFIG        _CONFIG1, B'00000110000100'                        ; int. oscillator and no code protection
-             __CONFIG        _CONFIG2, B'01100011111111'                        ;
-
+			org 000H					; After reset start here
+			goto setIntOsc				; Startover to init
+			org 004H						; an interrupt redirects this adress
+			goto ir_main
 
 ;-------------------------------------------------------------------------------
-;                             Timer Setup
+;		Init
 ;-------------------------------------------------------------------------------
 
-; Timer setup
-timer_init  movlw B'01111011'     			; Prescaler 1:64 / Postscaler 1:16
+setIntOsc	movlb 1						; Select BANK 1
+			movlw B'11100000'			; Set intern Osc to 2Mhz
+			movwf OSCCON				; Store in OSCCON Register
+
+initportA	movlb 0						; Select BANK 0
+			clrf PORTA					; Init PORTA
+			movlb 2						; Select BANK 2
+			clrf LATA
+			movlb 3						; Select BANK 3
+			clrf ANSELA					; all digital I/O
+			movlb 1						; Select BANK 1
+			movlw B'00111011'			; Set RA<5:3> and RA<1:0> s inputs
+			movwf TRISA					; and set RA2 as output
+			movlb 0						; Select BANK 0
+			bsf PORTA, RA2				; Set Output high
+
+initTimer	movlb 0						; Select BANK 0
+			movlw B'01111011'			; Postscaler 1:16, Prescaler 1:64, T2 OFF
 			movwf T2CON
-			clrf TMR2
-			movlw D'255'
-			movwf PR2                     	; Overflow after 255 timer pulses
+			clrf TMR2					; Start Timer from zero
+			movlw D'250'				; Counter overflow after 250 pulses
+			movwf PR2
 
-; Interrupt setup
-			banksel PIE1
-			clrf PIE1
+initIRQ		movlb 1						; Select BANK 1
+			clrf PIE1					; Disable all interrupt sources
 			clrf PIE2
-			bsf PIE1, TMR2IE              	; Enable timer 2 interrupt
-			clrf BSR                      	; select BANK0
-			movlw B'11000000'
-			movwf INTCON                  	; set global irq on
+			bsf PIE1, TMR2IE			; Enable Timer 2 IRQ
+			movlb 0						; Select BANK 0
+			movlw B'11000000'			; Enable interrupt global
+			movwf INTCON
 
-; Start Timer
-			bsf T2CON, D'002'             ; Set TMR2ON bit
-			return
+			; Start Timer2
+			bsf T2CON, TMR2ON
 
 ;-------------------------------------------------------------------------------
-;                             Main Loop
+;		Main Loop
 ;-------------------------------------------------------------------------------
 
-; Main loop do nothing and wait for timer irq
-main 		nop
-			goto main
-			
-;-------------------------------------------------------------------------------
-;                             Timer Interrupt
-;-------------------------------------------------------------------------------
+; Main loop do nothing than loop around and wait for timer interrupt
+main		nop
+			goto main					; Endless main loooooooop
 
-timer_ir	clrf BSR
+
+;-------------------------------------------------------------------------------
+;       Interrupt Routines
+;-------------------------------------------------------------------------------
+            
+; LED Switch interrupt routine
+; Bit GIE of INTCON is cleared in HW            
+ir_main  	movlb 0						; Select BANK 0 
+			btfsc PORTA, RA2			; Skip if port clear
+			goto ledsetlow
+			bsf PORTA, RA2				; Set output high
+			goto end_ir                 
+ledsetlow	bcf PORTA, RA2				; Set Output low
+
+			; Clear interrupt source for next run
+end_ir		clrf TMR2					; Start timer from zero
+			bcf PIR1, TMR2IF			; Clear timer 2 interrupt flag
+			retfie						; Sets bit GIE of INTCON too
 
 ;XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-                              END
+								END
